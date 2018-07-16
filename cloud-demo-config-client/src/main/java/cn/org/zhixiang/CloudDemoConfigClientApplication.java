3fa19b79ce6290d57b7e84ce017756cf7888b438
javen.shi <l>
2018-07-16 23:26:55 +0800

    配置中心

--- cloud-demo-config-client/src/main/java/cn/org/zhixiang/CloudDemoConfigClientApplication.java
+++ cloud-demo-config-client/src/main/java/cn/org/zhixiang/CloudDemoConfigClientApplication.java
@@ -1,15 +1,23 @@
 package cn.org.zhixiang;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 
 @SpringBootApplication
-@EnableConfigServer
-public class CloudDemoConfigServerApplication {
+@RestController
+public class CloudDemoConfigClientApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CloudDemoConfigServerApplication.class, args);
+		SpringApplication.run(CloudDemoConfigClientApplication.class, args);
+	}
+	@Value("${test}")
+	String test;
+	@RequestMapping(value = "/getTest")
+	public String getTest(){
+		return test;
 	}
 }
--- cloud-demo-config-server/src/main/java/cn/org/zhixiang/CloudDemoConfigServerApplication.java
+++ cloud-demo-config-server/src/main/java/cn/org/zhixiang/CloudDemoConfigServerApplication.java
@@ -2,24 +2,15 @@ package cn.org.zhixiang;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.ribbon.RibbonClient;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+
 
 @SpringBootApplication
-@EnableEurekaClient
-//@RibbonClient(name = "provider-demo", configuration = AvoidLoanbalanced.class)
-//@ComponentScan(excludeFilters = { @ComponentScan.Filter(type = FilterType.ANNOTATION, value = ExcludeFromComponentScan.class) })
-@RibbonClient(name = "provider-demo", configuration = cn.org.config.LoadBalanced.class)
-public class CloudDemoConsumerApplication {
-	@Bean
-	@LoadBalanced//开启负载均衡
-	public RestTemplate restTemplate(){
-		return new RestTemplate();
-	}
+@EnableConfigServer
+public class CloudDemoConfigServerApplication {
+
 	public static void main(String[] args) {
-		SpringApplication.run(CloudDemoConsumerApplication.class, args);
+		SpringApplication.run(CloudDemoConfigServerApplication.class, args);
 	}
 }
